5790be5fae5964ca7f9622a9cdaa2c0c5e09f0e5
joaoggs <joaogabrielggs@gmail.com>
2019-04-15 19:14:36 -0300

    Say commands

---
+++ commands/say.js
@@ -0,0 +1,32 @@
+const Discord = require("discord.js");
+const moongose = require("mongoose")
+
+module.exports.run = async (bot, message, args) => {
+    let msg = args.join(" ").slice(0);
+    let channelId = msg.slice(2,20);
+
+    //say msg (db channel)
+    bot.Guild.findOne({'guildId': message.guild.id}, (err, guild) => {
+
+        if(!guild.channel === "none"){
+            
+            let chat = message.guild.channels.find(`guildId`,guild.id)
+            return chat.send(msg)
+        }
+    })
+
+    //say #chat msg
+    if(!isNaN(channelId)){
+        let guildMention = message.guild.channels.get(channelId)
+        if(!message.member.hasPermission("ADMINISTRATOR") && guildMention) return message.channel.send("Need Permission");
+        let msgMention = args.join(" ").slice(22)
+        return guildMention.send(msgMention)
+    } 
+
+    //say msg
+    return message.channel.send(msg)   
+}
+
+module.exports.help = {
+    name: "say"
+}
