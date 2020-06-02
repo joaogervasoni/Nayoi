99e74268130438ca1812d040c88c046b48df93b7
Zaetic <joaogabrielggs@gmail.com>
2020-06-01 23:28:41 -0300

    Refatoração do say e sayembed

--- commands/says/say.js
+++ commands/says/say.js
@@ -1,39 +1,19 @@
-const {errorReturn} = require("../../functions.js");
+const { errorReturn, formatId } = require("../../functions.js");
 const { prefix } = require("../../botconfig.json");
 const ms = require("ms");
 
 module.exports.run = (bot, message, args) => {
     try{
         let mutetime = args[0];
+        let channel = message.guild.channels.cache.get(formatId(args[0]))
         mutetime = ms(mutetime);
         
-        if(!isNaN(mutetime) && isNaN(args[0].charAt(args[0].length-1))){
-            
-            if(message.guild.channels.cache.get(args[1].slice(2,20))){
-                let guildMention = message.guild.channels.cache.get(args[1].slice(2,20));
-    
-                setTimeout(function(){
-                    return guildMention.send(args.join(" ").slice(args[0].length + args[1].length + 1));
-                }, mutetime);
-
-            }
-            else{
-                setTimeout(function(){
-                    return message.channel.send(args.join(" ").slice(args[0].length));
-                }, mutetime);
-            }
+        if(channel){
+            return channel.send(args.join(" ").slice(args[0].length));
         }
         else{
-            if(message.guild.channels.cache.get(args[0].slice(2,20))){
-                let guildMention = message.guild.channels.cache.get(args[0].slice(2,20));
-    
-                return guildMention.send(args.join(" ").slice(args[0].length));
-            }
-            else{
-                return message.channel.send(args.join(" ").slice(0));
-            }
+            return message.channel.send(args.join(" ").slice(0));
         }
-          
     }catch(e){
         errorReturn(e, message, this.help.name)
     }
@@ -45,11 +25,6 @@ module.exports.help = {
     usability: "Pode ser utilizando usando `"+prefix+"say msgaqui`\n"
     +"Também pode ser mandando em um chat diferente usando`"+prefix+"say #channel msgaqui`\n"
     +"**Os comandos say e sayembed utilizam os mesmos parâmetros**\n",
-    additional: "Também é possivel adicionar tempo para uma msg ser enviada\n"
-    +"Utilize `"+prefix+"say 10s msgaqui`\n"
-    +"No caso de ter um channel `"+prefix+"say 10s #channel msgaqui`",
-    others: "O tempo deve ser expecificado com uma letra após o número para ser considerado um tempo valido\n"
-    +"**Exemplos:** `1s(seg) | 1m (min) | 1h (hora)`",
     type: "utilidade"
 }
 
--- commands/says/sayembed.js
+++ commands/says/sayembed.js
@@ -1,61 +1,29 @@
-const {errorReturn} = require("../../functions.js");
-const {MessageEmbed} = require("discord.js");
+const { errorReturn, formatId } = require("../../functions.js");
+const { MessageEmbed } = require("discord.js");
 const { prefix } = require("../../botconfig.json");
 const ms = require("ms");
 
 module.exports.run = (bot, message, args) => {
     try{
         let mutetime = args[0];
+        let channel = message.guild.channels.cache.get(formatId(args[0]))
         mutetime = ms(mutetime);
-        
-        if(!isNaN(mutetime) && isNaN(args[0].charAt(args[0].length-1))){
-            
-            if(message.guild.channels.cache.get(args[1].slice(2,20))){
-                let guildMention = message.guild.channels.cache.get(args[1].slice(2,20));
-                let msgSplit = args.join(" ").slice(args[0].length + args[1].length + 1).split("||");
-
-                setTimeout(function(){
-                    let embed = new MessageEmbed()
-                    .setTitle(msgSplit[0])
-                    .setDescription(msgSplit[1])
-                    .setColor(bot.baseColor)
-                    
-                    return guildMention.send(embed)
-                }, mutetime);
-
-            }
-            else{
-                let msgSplit = args.join(" ").slice(args[0].length).split("||");
-                setTimeout(function(){
-                    let embed = new MessageEmbed()
-                    .setTitle(msgSplit[0])
-                    .setDescription(msgSplit[1])
-                    .setColor(bot.baseColor)
-                    return message.channel.send(embed)
-                }, mutetime);
-            }
+    
+        if(channel){
+            let msgSplit = args.join(" ").slice(args[0].length).split("||");
+            let embed = new MessageEmbed()
+            .setTitle(msgSplit[0])
+            .setDescription(msgSplit[1])
+            .setColor(bot.baseColor)
+            return channel.send(embed)
         }
         else{
-
-            if(message.guild.channels.cache.get(args[0].slice(2,20))){
-                let guildMention = message.guild.channels.cache.get(args[0].slice(2,20));
-                
-                let msgSplit = args.join(" ").slice(args[0].length).split("||");
-
-                let embed = new MessageEmbed()
-                .setTitle(msgSplit[0])
-                .setDescription(msgSplit[1])
-                .setColor(bot.baseColor)
-                return guildMention.send(embed)
-            }
-            else{
-                let msgSplit = args.join(" ").slice(0).split("||");
-                let embed = new MessageEmbed()
-                .setTitle(msgSplit[0])
-                .setDescription(msgSplit[1])
-                .setColor(bot.baseColor)
-                return message.channel.send(embed)
-            }
+            let msgSplit = args.join(" ").slice(0).split("||");
+            let embed = new MessageEmbed()
+            .setTitle(msgSplit[0])
+            .setDescription(msgSplit[1])
+            .setColor(bot.baseColor)
+            return message.channel.send(embed)
         }
     }catch(e){
         errorReturn(e, message, this.help.name)
@@ -68,11 +36,6 @@ module.exports.help = {
     usability: "Pode ser utilizando usando `"+prefix+"sayembed título || msgaqui`\n"
     +"Também pode ser mandando em um chat diferente usando`"+prefix+"sayembed #channel título || msgaqui`\n"
     +"**Os comandos say e sayembed utilizam os mesmos parâmetros**\n",
-    additional: "Também é possivel adicionar tempo para uma msg ser enviada\n"
-    +"Utilize `"+prefix+"sayembed 10s título || msgaqui`\n"
-    +"No caso de ter um channel `"+prefix+"sayembed 10s #channel título || msgaqui`",
-    others: "O tempo deve ser expecificado com uma letra após o número para ser considerado um tempo valido\n"
-    +"**Exemplos:** `1s(seg) | 1m (min) | 1h (hora)`",
     type: "utilidade"
 }
 
