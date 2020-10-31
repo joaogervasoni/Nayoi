2223c79c3efe218bb7dbd6c22bcfab94d9fb7aa8
Zaetic <joaogabrielggs@gmail.com>
2020-10-31 11:36:05 -0300

    Adição de intents e atualização de packages

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const config = require("./botconfig.json");
 const NayoiClient = require("./nayoiClient.js");
 
 const bot = new NayoiClient(options = {
-    clientOption: {disableMentions: "everyone"},
+    clientOption: {
+        disableMentions: "everyone",
+        ws: { intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS'] }
+    },
     config: config
 });
 
